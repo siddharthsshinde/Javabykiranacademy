bc720fe46b04b86f22df0470052873bebc0e03df
Siddharth Shinde <93334709+siddharthsshinde@users.noreply.github.com>
2024-07-20 18:39:27 +0530

    Add files via upload

---
+++ Assignment 3/exp5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class exp5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number: ");
+        int num = sc.nextInt();
+        System.out.println("The number entered by user: " + num);
+    }
+}
---
+++ Assignment 3/exp6.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class exp6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter integer number ");
+        int a = sc.nextInt();
+        System.out.println("Enter String ");
+        String s = sc.next();
+        System.out.println("Enter integer is " + a + " And String is " + s);
+    }
+}
